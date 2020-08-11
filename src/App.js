d9160c789f36e70421c8037fb069ca7c63961a61
lauras12 <56985938+lauras12@users.noreply.github.com>
2020-08-11 11:06:26 -0400

    Updated Files

---
+++ src/App.js
@@ -0,0 +1,50 @@
+import React, { Component } from 'react';
+import Header from './Header/Header';
+import Features from './Features/Features';
+import Summary from './Summary/Summary'
+import './App.css';
+
+class App extends Component {
+  state = {
+    selected: {
+      Processor: {
+        name: '17th Generation Intel Core HB (7 Core with donut spare)',
+        cost: 700
+      },
+      'Operating System': {
+        name: 'Ubuntu Linux 16.04',
+        cost: 200
+      },
+      'Video Card': {
+        name: 'Toyota Corolla 1.5v',
+        cost: 1150.98
+      },
+      Display: {
+        name: '15.6" UHD (3840 x 2160) 60Hz Bright Lights and Knobs',
+        cost: 1500
+      }
+    }
+  };
+
+  updateFeature = (feature, newValue) => {
+    const selected = Object.assign({}, this.state.selected);
+    selected[feature] = newValue;
+    this.setState({
+      selected
+    });
+  };
+
+  render() {
+    return (
+      <div className="App">
+        <Header />
+        <main>
+          <Features list={this.props.features} selected={this.state.selected} onChange={this.updateFeature} />
+          <Summary selected={this.state.selected}/>  
+        </main>
+      </div>
+    );
+  }
+}
+
+export default App;
